8dd70e8cf4e3420bc3345ad2917ca24f323245fa
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-18 01:00:20 +0600

    set error page

--- src/Component/Page/Error/Error.jsx
+++ src/Component/Page/Error/Error.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import image from '../../../assets/error.webp'
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            <h1>404</h1>
+        <div className='text-center'>
+         <img src={image} alt="" />
+         <br />
+        <button className='btn btn-danger mt-3'> <Link to='/' className='text-decoration-none text-white fw-semibold'>Go to Home</Link></button>
         </div>
     );
 };
