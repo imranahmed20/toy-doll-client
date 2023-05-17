2c2ae81815ebf0be1d3df46dac755cc2c3ff586d
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-17 23:57:04 +0600

    router added and error page added

---
+++ src/Component/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Share/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Share/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Component/Layout/Main.jsx';
+import Home from './Component/Home/Home.jsx';
+import Error from './Component/Page/Error/Error.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
