a0566c72e4fb172d7b1189e92bd5b890c3bc1af5
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-18 00:15:05 +0600

    header added

--- src/Component/Share/Header/Header.jsx
+++ src/Component/Share/Header/Header.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../../../assets/logo.jpg'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <Container>
+            <Navbar bg="" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand className='fw-bold fs-4' href="#">
+                        <img src={logo} className='W-25 me-2 rounded-circle' alt="" style={{ width: 45 }} />
+                        Toy Doll</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="mx-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link className='fw-semibold mr-5' href="/">Home</Nav.Link>
+                            <Nav.Link className='fw-semibold mr-5' href="/allToy">All Toys</Nav.Link>
+                            <Nav.Link className='fw-semibold mr-5' href="/myToy">My Toy</Nav.Link>
+                            <Nav.Link className='fw-semibold mr-5' href="/addToy">Add A Toy</Nav.Link>
+                            <Nav.Link className='fw-semibold mr-5' href="/blog">Blog</Nav.Link>
+
+
+                        </Nav>
+                        <Form className="d-flex">
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
+
     );
 };
 
